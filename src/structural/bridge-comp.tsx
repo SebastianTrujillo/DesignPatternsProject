d82a3b351dbe3bb2a60c79f8453ef9387833a80a
SebastianTrujillo <juan.sebastian.rodriguez@correounivalle.edu.co>
2023-07-16 16:12:29 -0500

    2 files modified

--- src/structural/bridge-comp.tsx
+++ src/structural/bridge-comp.tsx
@@ -1,5 +1,62 @@
 import * as React from 'react';
 
+// Abstraction
+abstract class Abstraction {
+  protected implementor: Implementor;
+
+  constructor(implementor: Implementor) {
+    this.implementor = implementor;
+  }
+
+  public abstract operation(): void;
+}
+
+// Refined Abstraction
+class RefinedAbstraction extends Abstraction {
+  constructor(implementor: Implementor) {
+    super(implementor);
+  }
+
+  public operation(): void {
+    console.log('RefinedAbstraction operation');
+    this.implementor.implementation();
+  }
+}
+
+// Implementor
+interface Implementor {
+  implementation(): void;
+}
+
+// Concrete Implementor A
+class ConcreteImplementorA implements Implementor {
+  public implementation(): void {
+    console.log('ConcreteImplementorA implementation');
+  }
+}
+
+// Concrete Implementor B
+class ConcreteImplementorB implements Implementor {
+  public implementation(): void {
+    console.log('ConcreteImplementorB implementation');
+  }
+}
+
 export const BridgeComp = () => {
+  // Usage
+  const implementorA = new ConcreteImplementorA();
+  const abstractionA = new RefinedAbstraction(implementorA);
+  abstractionA.operation();
+  // Output:
+  // RefinedAbstraction operation
+  // ConcreteImplementorA implementation
+
+  const implementorB = new ConcreteImplementorB();
+  const abstractionB = new RefinedAbstraction(implementorB);
+  abstractionB.operation();
+  // Output:
+  // RefinedAbstraction operation
+  // ConcreteImplementorB implementation
+
   return <h2>{'Bridge'}</h2>;
 };
--- src/structural/facade-comp.tsx
+++ src/structural/facade-comp.tsx
@@ -1,5 +1,50 @@
 import * as React from 'react';
 
+// Subsystem 1
+class Subsystem1 {
+  public operation1(): void {
+    console.log('Subsystem 1: Operation 1');
+  }
+
+  public operation2(): void {
+    console.log('Subsystem 1: Operation 2');
+  }
+}
+
+// Subsystem 2
+class Subsystem2 {
+  public operation1(): void {
+    console.log('Subsystem 2: Operation 1');
+  }
+
+  public operation2(): void {
+    console.log('Subsystem 2: Operation 2');
+  }
+}
+
+// Facade
+class Facade {
+  private subsystem1: Subsystem1;
+  private subsystem2: Subsystem2;
+
+  constructor() {
+    this.subsystem1 = new Subsystem1();
+    this.subsystem2 = new Subsystem2();
+  }
+
+  public operation(): void {
+    console.log('Facade operation:');
+    this.subsystem1.operation1();
+    this.subsystem1.operation2();
+    this.subsystem2.operation1();
+    this.subsystem2.operation2();
+  }
+}
+
 export const FacadeComp = () => {
+  // Usage
+  const facade = new Facade();
+  facade.operation();
+
   return <h2>{'Facade'}</h2>;
 };
