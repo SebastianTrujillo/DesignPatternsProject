955d2691d6204b739cde94c34f4bcefd3aa45057
SebastianTrujillo <juan.sebastian.rodriguez@correounivalle.edu.co>
2023-07-16 13:07:33 -0500

    2 files modified

--- src/creational/factory-method.tsx
+++ src/creational/factory-method.tsx
@@ -1,5 +1,47 @@
 import * as React from 'react';
 
+// Product interface
+interface Product {
+  operation(): void;
+}
+
+// Concrete product 1
+class ConcreteProduct1 implements Product {
+  operation(): void {
+    console.log('ConcreteProduct1 operation');
+  }
+}
+
+// Concrete product 2
+class ConcreteProduct2 implements Product {
+  operation(): void {
+    console.log('ConcreteProduct2 operation');
+  }
+}
+
+// Creator class
+class Creator {
+  createProduct(type: string): Product {
+    switch (type) {
+      case 'product1':
+        return new ConcreteProduct1();
+      case 'product2':
+        return new ConcreteProduct2();
+      default:
+        throw new Error('Invalid product type');
+    }
+  }
+}
+
 export const FactoryMethod = () => {
+  // Usage
+  const creator = new Creator();
+
+  const product1 = creator.createProduct('product1');
+  product1.operation(); // Output: ConcreteProduct1 operation
+
+  const product2 = creator.createProduct('product2');
+  product2.operation(); // Output: ConcreteProduct2 operation
+
   return <h2>{'Factory Method'}</h2>;
 };
--- src/creational/prototype.tsx
+++ src/creational/prototype.tsx
@@ -1,5 +1,36 @@
 import * as React from 'react';
 
+// Prototype interface
+interface Prototype {
+  clone(): Prototype;
+}
+
+// Concrete prototype
+class ConcretePrototype implements Prototype {
+  private property: string;
+
+  constructor(property: string) {
+    this.property = property;
+  }
+
+  public clone(): Prototype {
+    // Create a new instance of the same concrete prototype class
+    const clone = new ConcretePrototype(this.property);
+    return clone;
+  }
+
+  public getProperty(): string {
+    return this.property;
+  }
+}
+
 export const Prototype = () => {
+  // Usage
+  const prototype = new ConcretePrototype('Prototype');
+  const clone = prototype.clone();
+
+  console.log(prototype.getProperty()); // Output: Prototype
+  console.log(clone.getProperty()); // Output: Prototype
+
   return <h2>{'Prototype'}</h2>;
 };
